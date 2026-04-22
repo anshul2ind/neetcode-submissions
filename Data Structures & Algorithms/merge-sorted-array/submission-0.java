69aa0c6141b5321e7af430213f61370ea3d7119a
anshul2ind <46841052+anshul2ind@users.noreply.github.com>
2026-04-22 18:06:22 +0530

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.java
@@ -0,0 +1,56 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < nums2.length; i++) {
+            nums1[m + i] = nums2[i];
+        }
+
+        // Arrays.sort(nums1);
+        intertionSort(nums1);
+    }
+
+    public void bubbleSort(int[] num) {
+        int len = num.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (num[j] > num[j + 1]) {
+                    int temp = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void selectionSort(int[] num) {
+        int len = num.length;
+        int mini;
+
+        for (int i = 0; i < len - 1; i++) {
+            mini = i;
+            for (int j = i + 1; j < len; j++) {
+                if (num[j] < num[mini]) {
+                    mini = j;
+                }
+            }
+
+            int temp = num[mini];
+            num[mini] = num[i];
+            num[i] = temp;
+        }
+    }
+
+    public void intertionSort(int[] num) {
+        int len = num.length;
+        for (int i = 0; i < len; i++) {
+            int temp = num[i];
+            int j = i;
+
+            while (j > 0 && temp < num[j-1]) {
+                num[j] = num[j - 1];
+                j--;
+            }
+
+            num[j] = temp;
+        }
+    }
+}
