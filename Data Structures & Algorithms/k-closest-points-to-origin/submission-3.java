e561a1a76df5944c8c989641109a31fe09d4c1c6
anshul2ind <46841052+anshul2ind@users.noreply.github.com>
2026-05-28 17:56:54 +0530

    Add: k-closest-points-to-origin - submission-3

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-3.java
@@ -0,0 +1,28 @@
+public record PointWithDis(Integer x, Integer y, Double diss) {}
+
+class Solution {
+
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<PointWithDis> queue = new PriorityQueue(
+            Comparator.comparingDouble(PointWithDis::diss).reversed()
+        );
+
+        for(var point: points) {
+            var x = point[0];
+            var y = point[1];
+            var diss = Math.sqrt(x*x + y*y);
+            queue.offer(new PointWithDis(x,y, diss));
+            if(queue.size() > k) {
+                queue.poll();
+            }
+        }
+
+        int[][] result = new int[queue.size()][2];
+        for(int i = 0; i < result.length; i++) {
+            var point = queue.poll();
+            result[i] = new int[] {point.x(), point.y()};
+        }
+        return result;
+    }
+
+}
