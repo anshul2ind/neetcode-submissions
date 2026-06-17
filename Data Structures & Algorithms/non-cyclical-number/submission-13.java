966351e05e6b17639f0e565e1d22917154468b96
anshul2ind <46841052+anshul2ind@users.noreply.github.com>
2026-06-17 14:46:18 +0530

    Add: non-cyclical-number - submission-13

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-13.java
@@ -0,0 +1,27 @@
+class Solution {
+    //312
+    //31
+    //3
+    //.3
+
+    public int sumOfDigits(int n) {
+        var sum = 0;
+        while(n >= 1) {
+            var digit = n%10;
+            sum += (digit * digit);
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public boolean isHappy(int n) {
+       int slow = n;
+       int fast = sumOfDigits(n);
+
+       while(fast != 1 && slow != fast) {
+        slow = sumOfDigits(slow);
+        fast = sumOfDigits(sumOfDigits(fast));
+       }
+       return fast == 1;
+    }
+}
