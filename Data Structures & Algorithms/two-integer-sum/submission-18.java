82ca31b93e42fc87b1a344cdbc4c93dac1cc22aa
anshul2ind <46841052+anshul2ind@users.noreply.github.com>
2026-08-10 12:37:31 +0530

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.java
@@ -0,0 +1,47 @@
+class Solution {
+    class Node {
+        int index;
+        int val;
+        Node(int index, int val) {
+            this.index = index;
+            this.val = val;
+        }
+    }
+    public int[] twoSum(int[] nums, int target) {
+        
+        int l  = 0;
+        int r = nums.length-1;
+    
+        Node[] arr = new Node[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            arr[i] = new Node(i, nums[i]);
+        }
+
+        BSort(arr);
+
+        while(l < r) {
+            int sum = arr[l].val + arr[r].val;
+            if(sum < target) {
+                l++;
+            } else if (sum > target) {
+                r--;
+            } else {
+                return new int[] {Math.min(arr[l].index,arr[r].index), Math.max(arr[l].index,arr[r].index)};
+            }
+        }
+        return null;
+    }
+
+    public void BSort(Node[] nums) {
+        for(int i = 0; i < nums.length -1; i++) {
+            for(int j = 0; j < nums.length-i-1; j++) {
+                if(nums[j].val > nums[j+1].val) {
+                    var temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+}
+
