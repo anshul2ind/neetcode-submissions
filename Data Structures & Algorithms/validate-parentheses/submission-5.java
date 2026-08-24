e33ae3bf201102f8150860fa68c5627f45611de3
anshul2ind <46841052+anshul2ind@users.noreply.github.com>
2026-08-24 06:54:30 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> map = Map.of(
+            ')', '(',
+            ']', '[',
+            '}', '{'
+        );
+
+        for(var ch: s.toCharArray()) {
+            if(map.containsKey(ch)) {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+               char poped = stack.pop();
+               if(poped != map.get(ch)) {
+                return false;
+               }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
